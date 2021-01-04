60ce82e8534bec55347cc8d1215e901bde0c4f68
llxor <llxoyd@gmail.com>
2021-01-04 09:38:26 +0000

    Refactor argv to use []

--- factor.c
+++ factor.c
@@ -105,7 +105,7 @@ void sort(int len, long arr[len]) {
   }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char argv[][]) {
   for (int index = 1; index < argc; ++index) {
     long n;
     sscanf(argv[index], "%ld", &n);
